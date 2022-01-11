9015f3077bced7621edf76aadcda3bbe725eebca
iamryoman <iamryoman@gmail.com>
2022-01-11 13:17:18 +0900

    Replace KeybordAvoidingView with KeyboardSafeView

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <MemoEditScreen />
   );
 }
