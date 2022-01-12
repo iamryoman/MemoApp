25aff7e6cbf3ecd200940d9d476d2f367a7050c3
iamryoman <iamryoman@gmail.com>
2022-01-12 16:09:14 +0900

    Inplement signup and login

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
 import {
-  StyleSheet, View, Text, TextInput, TouchableOpacity,
+  StyleSheet, View, Text, TextInput, TouchableOpacity, Alert,
 } from 'react-native';
-
+import firebase from 'firebase';
 import Button from '../components/Button';
 
 export default function LogInScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  function handlePress() {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const { user } = userCredential;
+        console.log(user.uid);
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'MemoList' }],
+        });
+      })
+      .catch((error) => {
+        Alert.alert(error.code);
+      });
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
@@ -33,12 +49,7 @@ export default function LogInScreen(props) {
         />
         <Button
           label="submit"
-          onPress={() => {
-            navigation.reset({
-              index: 0,
-              routes: [{ name: 'MemoList' }],
-            });
-          }}
+          onPress={handlePress}
         />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Not registered?</Text>
