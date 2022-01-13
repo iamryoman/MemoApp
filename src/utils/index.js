b893b49562ab9641507fc50446da5f4374dc1e23
iamryoman <iamryoman@gmail.com>
2022-01-13 19:42:21 +0900

    Fetch data from firestore on MemoDetailScreen

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import { format } from 'date-fns';
+
+export function dateToString(date) {
+  if (!date) { return ''; }
+  return format(date, 'yyyy年M月d日 HH時MM分');
+};
