73c12b7826a80b0b31c4fc871f8e84ca28307b6a
iamryoman <iamryoman@gmail.com>
2022-01-14 12:35:19 +0900

    Create Loading component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
-import { func, string } from 'prop-types';
+import { func, shape, string } from 'prop-types';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function Button(props) {
-  const { label, onPress } = props;
+  const { label, onPress, style } = props;
   return (
-    <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
+    <TouchableOpacity style={[styles.buttonContainer, style]} onPress={onPress}>
       <Text style={styles.buttonLabel}>{label}</Text>
     </TouchableOpacity>
   );
@@ -13,10 +13,12 @@ export default function Button(props) {
 Button.propTypes = {
   label: string.isRequired,
   onPress: func,
+  style: shape(),
 };
 
 Button.defaultProps = {
   onPress: null,
+  style: null,
 };
 
 const styles = StyleSheet.create({
